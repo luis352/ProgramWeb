228388a1b0eff7f0dd00aec65669fdf8e2f1492c
Luis Rendon <lrendon@gemcousa.com>
2023-10-02 07:46:21 -0500

    moduloECMAJavascrip

---
+++ Parcial2/ModuloECMA/Areas.js
@@ -0,0 +1,28 @@
+export function AreaCuadrado(Lado)
+{
+    return  Lado*Lado;
+}
+
+export function AreaTringualo(Base, altura){
+    return ((Base*altura)/2 ); 
+}
+
+export function saluda() {
+    console.log("Hola desde el módulo!");
+}
+
+
+
+
+
+
+
+// module.exports.AreaCuadrado=AreaCuadrado;
+// module.exports.AreaTringualo=AreaTringualo;
+
+
+
+
+// console.log(_dirname)
+// console.log(module);
+// console.log(_filname)
---
+++ Parcial2/ModuloECMA/CalculoAreas.js
@@ -0,0 +1,22 @@
+
+import * as areas from "./Areas.js"
+// import * as cowsay from "cowsay"
+
+let a=areas.AreaCuadrado(4)
+
+let b=areas.AreaTringualo(4 , 10)
+
+
+
+
+console.log(a, b );
+
+
+
+
+
+
+
+
+
+
