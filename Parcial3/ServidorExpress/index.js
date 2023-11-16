fcd13cdce139ac9f37f1e986b98cc1b1be4c5d6f
Luis Rendon <lrendon@gemcousa.com>
2023-11-15 22:39:00 -0600

    Cors Y backend

--- Parcial3/ServidorExpress/index.js
+++ Parcial3/ServidorExpress/index.js
@@ -1,19 +1,3 @@
-// const express=require('express');
-// const app=express('cors');
-// const cors = require('cors')
-// app.get('/',(req,res)=>{
-//     res.json({mensaje:"Server express respondiendo a get"});
-// });
- 
-// app.post('/',(req,res)=>{
-//     res.json({mensaje:"Server express respondiendo a post"})
-// })
-// app.delete('/',(req,res)=>{
-//     res.json({mensaje:"Server express respondiendo a delete"})
-// })
-// app.listen(8082,()=>{
-//     console.log("Servidor express en puerto 8082")
-// });
 
 
 const express = require('express');
